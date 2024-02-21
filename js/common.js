0fc8e5499a749ae26c7e0e325fe6d0cbeff96e85
SushilSylvester <50085377+SushilSylvester@users.noreply.github.com>
2024-02-21 21:55:54 +0530

    changes

--- js/common.js
+++ js/common.js
@@ -290,49 +290,44 @@ Function Scroll Effects
 		
 		
 		// Horizontal Gallery
-		const panelsSections = gsap.utils.toArray( ".panels" );
-		for (var i = 0; i < panelsSections.length; i++){
-			
-			thePanelsSection = panelsSections[i];
-			const panels = gsap.utils.toArray(".panels-container .panel", thePanelsSection );
-			const panelsImgs = gsap.utils.toArray(".panels-container .panel img", thePanelsSection );
+		const panelsSections = gsap.utils.toArray(".panels");
+		for (let i = 0; i < panelsSections.length; i++) {
+			const thePanelsSection = panelsSections[i];
+			const panels = gsap.utils.toArray(".panels-container .panel", thePanelsSection);
+			const panelsImgs = gsap.utils.toArray(".panels-container .panel img", thePanelsSection);
 			const panelsContainer = thePanelsSection.querySelector(".panels-container");
 			const widthRatio = thePanelsSection.dataset.widthratio;
-			
+
 			gsap.set([panelsContainer, panels], { height: window.innerHeight * 0.6 });
 			gsap.set(panels, { width: window.innerHeight * widthRatio });
-			
-			
-					
-			var totalPanelsWidth = 0;
-			panels.forEach(function( panel )  {
-				totalPanelsWidth += $(panel).outerWidth(true);
+
+			let totalPanelsWidth = 0;
+			panels.forEach(panel => {
+				totalPanelsWidth += panel.offsetWidth;
 			});
-			gsap.set(panelsContainer, {width:totalPanelsWidth});
-			
+			gsap.set(panelsContainer, { width: totalPanelsWidth });
+
 			gsap.set(thePanelsSection, { height: panelsContainer.offsetWidth - innerWidth + panelsContainer.offsetHeight });
-			
+
 			gsap.to(panels, {
-				x: - totalPanelsWidth + innerWidth,
+				x: -totalPanelsWidth + innerWidth,
 				ease: "none",
 				scrollTrigger: {
 					trigger: panelsContainer,
 					pin: true,
-					start: function() {
+					start: () => {
 						const startPin = (window.innerHeight - panelsContainer.offsetHeight) / 2;
 						return "top +=" + startPin;
 					},
-					end: function() {
+					end: () => {
 						const endPin = panelsContainer.offsetWidth - innerWidth;
 						return "+=" + endPin;
 					},
 					scrub: 1,
 				}
-			});			
-		}	
-			
-		
-		
+			});
+		}
+
 		// Slowed Pin Section
 		gsap.utils.toArray('.slowed-pin').forEach((slowedPin) => {
 			
